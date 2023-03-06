21e4b31cae7dfe813a73d28eae590eb8cc355c52
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-03-06 18:28:14 -0500

    js geo, html start

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ var oldCity = "";
 
 
 
+//direct geocoding
+http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
 
-
-// Get the latitude and longitude from Open Weather Maps API
+//latitude and longitude
 let latitude = response.coord.lat;
 let longitude = response.coord.lon;
 let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&APPID=" + owkAPI;
@@ -16,3 +17,6 @@ let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon="
 
 //latitude and longitude ^^^^
 http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}
+
+// zip code
+http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
