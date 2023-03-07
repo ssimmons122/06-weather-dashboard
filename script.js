4a89638cf8ecf629a4403fe6b512f8ca27998e0d
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-03-06 23:13:50 -0500

    event listener, API, history JS

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 // set variables
-var owkAPI = "e39446cd1b964186c052e34bedb04020";
-var newCity = "";
-var oldCity = "";
+//var owkAPI = "e39446cd1b964186c052e34bedb04020";
+//var newCity = "";
+//var oldCity = "";
 
+
+$(document).ready(function () {
+    //search btn
+    $("#search-button").on("click", function () {
+      var searchTerm = $("#search-value").val();
+      //empty input field.
+      $("#search-value").val("");
+      weatherFunction(searchTerm);
+      weatherForecast(searchTerm);
+    });
+
+//save city
+function saveCity(text) {
+        var listItem = $("<li>").addClass("list-group-item").text(text);
+        $(".history").append(listItem);
+      }
+    
+//listener for li on click 
+      $(".history").on("click", "li", function () {
+        weatherFunction($(this).text());
+        weatherForecast($(this).text());
+      });
+
+function weatherFunction(searchTerm) {
+
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=e39446cd1b964186c052e34bedb04020",
+    
+    }).then(function (data) {
+        //if index of search value does not exist
+        if (history.indexOf(searchTerm) === -1) {
+          //push searchValue to history array
+          history.push(searchTerm);
+          //places item pushed into local storage
+          localStorage.setItem("history", JSON.stringify(history));
+          createRow(searchTerm);
+        }
 
 
 //direct geocoding
