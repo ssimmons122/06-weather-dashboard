3891f5a4269bd4ed6df88b63f3d507893c2d6285
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-03-08 00:12:33 -0500

    css js clean up

--- script.js
+++ script.js
@@ -1,93 +1,69 @@
 // set variables
 var owkAPI = "e39446cd1b964186c052e34bedb04020";
-var newCity = "";
-var oldCity = "";
+var inputEl = document.querySelector('.input');
+var citiesListEl = document.querySelector(".cities-list");
+var travelCity = localStorage.getItem('travelCityStore'); //save searched city
+var lat = coord.lat;
+var lon = coord.lon;
 
+// city search button event listener
+searchBtnEl.addEventListener('click', recordCityData);
 
-// New city search button event listener
-$('#search-button').on("click", (event) => {
-    event.preventDefault();
-    newCity = $('#search-city').val();
-    getCurrentConditions(event);
-    });
+// current city big cont
+var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + travelCity + "&units=metric" + owkAPI ;
 
+//5 day weather
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + travelCity + "&units=metric" + owkAPI;
 
-var getCurrentConditions = (event) => {
-        // searched city
-    let city = $('#search-city').val();
-    currentCity= $('#search-city').val();
-        // Set queryURL to fetch from API using weather search
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&APPID=" + owkAPI;
-        fetch(queryURL)
-        .then((response) => {
-          return response.json();
-        })
-    
-    .then((response) => {
-        // Save city to local storage
-     saveCity(city);
-        $('#search-error').text("");
-        // Create icon for the current weather using Open Weather Maps
-    let currentWeatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
-
-        
-        // Render cities list
-        renderCities();
-        // Obtain the 5day forecast for the searched city
-        getFiveDayForecast(event);
-        // Set the header text to the found city name
-        $('#header-text').text(response.name);
-        // HTML for the results of search
-        let currentWeatherHTML = `
-        <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${currentWeatherIcon}"></h3>
-        <ul class="list-unstyled">
-            <li>Temperature: ${response.main.temp}&#8457;</li>
-            <li>Humidity: ${response.main.humidity}%</li>
-            <li>Wind: ${response.wind.speed} mph</li>
-            <li id="uvIndex">UV Index:</li>
-        </ul>`;
-    // Append the results to the DOM
-$('#current-weather').html(currentWeatherHTML);
-
-// rev geocode
-    let latitude = response.coord.lat;
-    let longitude = response.coord.lon;
-    let revQueryURL = "api.openweathermap.org/geo/1.0/reverse?lat=" + latitude + "&lon=" + longitude + "&APPID=" + owkAPI;
-    });
+function recordCityData() {
+    localStorage.setItem('travelCityStore', inputEl.value);
 }
 
+// Append the search input from localStorage to the cities list
+for (var i = 0; i < localStorage.length; i++) {
+    $(".cities-list").append("<p>" + localStorage.getItem(localStorage.key(i)) + "</p>");
+}
 
-//list of searched cities
-var renderCities = () => {
-    $('#city-results').empty();
-    // If localStorage is empty
-    if (localStorage.length===0){
-        if (oldCity){
-            $('#search-city').attr("value", oldCity);
-        } else {
-            $('#search-city').attr("value", "Columbus");
-        }
-    } else {
-        // Build key of last city written to localStorage
-        let lastCityKey="cities"+(localStorage.length-1);
-        oldCity=localStorage.getItem(lastCityKey);
-        // Set search input to last city searched
-        $('#search-city').attr("value", lastCity);
-        // Append stored cities to page
-        for (let i = 0; i < localStorage.length; i++) {
-            let city = localStorage.getItem("cities" + i);
-            let cityEl;
-            // Set to oldCity if newCIty not set
-            if (newCity===""){
-                newCity=oldCity;
-            }
-            // Set button class to active for currentCity
-            if (city === newCity) {
-                cityEl = `<button type="button" class="list-group-item list-group-item-action active">${city}</button></li>`;
-            } else {
-                cityEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`;
-            } 
-            // Append city to page
-            $('#city-results').prepend(cityEl);
-        }
-    }}
+// Current Day Forecast function
+$.ajax ({
+    url: forecastURL,
+    method: "GET"
+})
+.then(function (response) {
+
+    var dayOne = moment(response.list[0].dt_txt).format("ddd, MMM D");
+    // day 1 
+    $(".day-one-temperature").text("Temp: " + response.list[0].main.temp + " F");
+    $(".day-one-date").html("<h6>" + dayOne + "</h6>");
+    $(".day-one-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+    $(".day-one-humidity").text("Humidity: " + response.list[0].main.humidity + "%");
+
+    var dayTwo = moment(response.list[8].dt_txt).format("ddd, MMM D");
+    // day 2 
+    $(".day-two-temperature").text("Temp: " + response.list[8].main.temp + " F");
+    $(".day-two-date").html("<h6>" + dayTwo + "</h6>");
+    $(".day-two-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[8].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+    $(".day-two-humidity").text("Humidity: " + response.list[8].main.humidity + "%");
+
+    var dayThree = moment(response.list[16].dt_txt).format("ddd, MMM D");
+    // day 3 
+    $(".day-three-temperature").text("Temp: " + response.list[16].main.temp + " F");
+    $(".day-three-date").html("<h6>" + dayThree + "</h6>");
+    $(".day-three-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[16].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+    $(".day-three-humidity").text("Humidity: " + response.list[16].main.humidity + "%");
+
+    var dayFour = moment(response.list[24].dt_txt).format("ddd, MMM D");
+    // day 4 
+    $(".day-four-temperature").text("Temp: " + response.list[24].main.temp + " F");
+    $(".day-four-date").html("<h6>" + dayFour + "</h6>");
+    $(".day-four-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[24].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+    $(".day-four-humidity").text("Humidity: " + response.list[24].main.humidity + "%");
+
+    var dayFive = moment(response.list[32].dt_txt).format("ddd, MMM D");
+    // day 5 
+    $(".day-five-temperature").text("Temp: " + response.list[32].main.temp + " F");
+    $(".day-five-date").html("<h6>" + dayFive + "</h6>");
+    $(".day-five-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[32].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+    $(".day-five-humidity").text("Humidity: " + response.list[32].main.humidity + "%");
+
+});
